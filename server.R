b5eeb064151bf3002572f87c2fded8ae5200d89c
Rafia29 <90558454+Rafia29@users.noreply.github.com>
2021-10-22 20:44:00 +0600

    Update server.R

--- server.R
+++ server.R
@@ -1,60 +1,145 @@
 
 
+# Define server logic required to draw a histogram
 server <- function(input, output) {
-  combined.bangladesh <- reactive({
-    invalidateLater(1000 * 60 * 60 * 24)
-    get.data()
+  # Create reactive input
+  popsize <- 21741090 #Population of Dhaka, Bangladesh
+  dataInput <- reactive({
+    init       <-
+      c(
+        S = 1 - input$pinf / popsize - input$pvac / 100 * input$vaceff / 100,
+        I = input$pinf / popsize,
+        R = 0,
+        V = input$pvac / 100 * input$vaceff / 100
+      )
+    ## beta: infection parameter; gamma: recovery parameter
+    parameters <-
+      c(beta = input$connum * 1 / input$infper,
+        # * (1 - input$pvac/100*input$vaceff/100),
+        gamma = 1 / input$infper)
+    ## Time frame
+    times      <- seq(0, input$timeframe, by = .2)
+    
+    ## Solve using ode (General Solver for Ordinary Differential Equations)
+    out <- ode(
+        y = init,
+        times = times,
+        func = sir,
+        parms = parameters
+      )   
+    #    out
+    as.data.frame(out)
   })
-  output$plot1 <-
-    renderPlot({
-      ggplot(data = combined.bangladesh(), aes(x = Date, y = People_fully_vaccinated)) +
-        geom_line(color = "#339999", size = 1.25) + xlab("") + ylab('People fully vaccinated\n') +
-        scale_x_date(date_breaks = "1 week", date_labels = "%d %b %Y") +
-        theme(axis.text.x = element_text(
-          angle = 45,
-          vjust = 0.5,
-          hjust = 1
-        )) +
-        scale_y_continuous(labels = comma) +
-        theme(text = element_text(size = rel(4)))
-    })
-  output$plot3 <-
-    renderPlot({
-      ggplot(data = combined.bangladesh(), aes(x = Date, y = Daily.Cases)) +
-        geom_line(color = "#FF9999", size = 1.25) + xlab("") + ylab('Daily cases\n') +
-        scale_x_date(date_breaks = "1 week", date_labels = "%d %b %Y") +
-        theme(axis.text.x = element_text(
-          angle = 45,
-          vjust = 0.5,
-          hjust = 1
-        )) +
-        scale_y_continuous(labels = comma) +
-        theme(text = element_text(size = rel(4)))
-    })
-  output$plot2 <-
-    renderPlot({
-      ggplot(data = combined.bangladesh(), aes(x = Date, y = People_partially_vaccinated)) +
-        geom_line(color = "#FF9933", size = 1.25) + xlab("") + ylab('People partially vaccinated \n') +
-        scale_x_date(date_breaks = "1 week", date_labels = "%d %b %Y") +
-        theme(axis.text.x = element_text(
-          angle = 45,
-          vjust = 0.5,
-          hjust = 1
-        )) +
-        scale_y_continuous(labels = comma) +
-        theme(text = element_text(size = rel(4)))
-    })
-  output$plot4 <-
-    renderPlot({
-      ggplot(data = combined.bangladesh(), aes(x = Date, y = Daily.Deaths)) +
-        geom_line(color = "#CC0000", size = 1.25) + xlab("") + ylab('Daily Deaths \n') +
-        scale_x_date(date_breaks = "1 week", date_labels = "%d %b %Y") +
-        theme(axis.text.x = element_text(
-          angle = 45,
-          vjust = 0.5,
-          hjust = 1
-        )) +
-        scale_y_continuous(labels = comma) +
-        theme(text = element_text(size = rel(4)))
-    })
+  
+  output$distPlot <- renderPlot({
+    out <-
+      dataInput() %>%
+      gather(key, value, -time) %>%
+      mutate(
+        id = row_number(),
+        key2 = recode(
+          key,
+          S = "Susceptible (S)",
+          I = "Infected (I)",
+          R = "Recovered (R)",
+          V = "Vaccinated / immune (V)"
+        ),
+        keyleft = recode(
+          key,
+          S = "Susceptible (S)",
+          I = "",
+          R = "",
+          V = "Vaccinated / immune (V)"
+        ),
+        keyright = recode(
+          key,
+          S = "",
+          I = "Infected (I)",
+          R = "Recovered (R)",
+          V = ""
+        )
+      )
+    
+    ggplot(data = out,
+           aes(
+             x = time,
+             y = value,
+             group = key2,
+             col = key2,
+             label = key2,
+             data_id = id
+           )) + # ylim(0, 1) +
+      ylab("Proportion of full population") + xlab("Time (days)") +
+      geom_line(size = 2) +
+      geom_text_repel(
+        data = subset(out, time == max(time)),
+        aes(label = keyright),
+        size = 6,
+        segment.size  = 0.2,
+        segment.color = "grey50",
+        nudge_x = 0,
+        hjust = 1,
+        direction = "y"
+      ) +
+      geom_text_repel(
+        data = subset(out, time == min(time)),
+        aes(label = keyleft),
+        size = 6,
+        segment.size  = 0.2,
+        segment.color = "grey50",
+        nudge_x = 0,
+        hjust = 0,
+        direction = "y"
+      ) +
+      theme(legend.position = "none") +
+      scale_colour_manual(values = c("red", "green4", "black", "blue")) +
+      scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
+      theme(
+        rect=element_rect(size=0),
+        legend.position="none",
+        panel.background=element_rect(fill="transparent", colour=NA),
+        plot.background=element_rect(fill="transparent", colour=NA),
+        legend.key = element_rect(fill = "transparent", colour = "transparent")
+      )
+    
+  })
+  
+  output$progressBox <- renderValueBox({
+    valueBox(
+      dataInput() %>% filter(time == max(time)) %>% select(R) %>% mutate(R = round(100 * R, 2)) %>% paste0("%"),
+      "Proportion of full population that got the disease by end of time frame",
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "black"
+    )
+  })
+  
+  output$approvalBox <- renderValueBox({
+    valueBox(
+      paste0(round(
+        100 * (dataInput() %>% filter(row_number() == n()) %>% mutate(res = (R + I) / (S + I + R)) %>% pull("res")), 2), "%"),
+      "Proportion of susceptibles that will get the disease by end of time frame",
+      icon = icon("thermometer-full"),
+      color = "black"
+    )
+  })
+  
+  output$BRRBox <- renderValueBox({
+    valueBox(
+      paste0(round(input$connum *
+                     (1 - input$pvac / 100 * input$vaceff / 100), 2), ""),
+      "Effective R0 (for populationen at outbreak, when immunity is taken into account)",
+      icon = icon("arrows-alt"),
+      color = "red"
+    )
+  })
+  
+  output$HIBox <- renderValueBox({
+    valueBox(
+      paste0(round(100 * (1 - 1 / (input$connum)), 2), "%"),
+      "Proportion of population that needs to be immune for herd immunity",
+      icon = icon("medkit"),
+      color = "blue"
+    )
+  })  
 }
+
